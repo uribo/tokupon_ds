df07666bc862597cf923a1aaf2a2a9120c99ffe2
Shinya Uryu <uryu.shinya@tokushima-u.ac.jp>
2022-06-22 16:32:52 +0900

    [fix]出力先を_bookフォルダに変更

--- scripts/build_website.R
+++ scripts/build_website.R
@@ -3,7 +3,7 @@ on_ci <- isTRUE(as.logical(Sys.getenv("CI")))
 # Build website --------------
 build_website <- function() {
   cli::cli_h2("Building website")
-  site_output <- "_site"
+  site_output <- "_book"
   unlink(site_output, recursive = TRUE)
   cli::cli_alert_info("Running quarto to build website")
   quarto::quarto_render(as_job = FALSE)
@@ -11,4 +11,4 @@ build_website <- function() {
 
 build_website()
 
-if (!on_ci) servr::httd("_site")
+if (!on_ci) servr::httd("_book")
